af04e5ba6ecff434517c3cbb50bf3a7a083bf412
Juan Miguel <jmcastrom@eafit.edu.co>
2024-05-02 17:47:09 -0500

    Resolving data conflicts

--- app/details/page.js
+++ app/details/page.js
@@ -1,12 +1,13 @@
 import Details from "./Details";
 import Title from "./Title";
+import data from "../listingsData.json";
 
 export default async function Page() {
 
     return (
         <div className="flex flex-col min-h-screen">
             <div>
-                <Title />
+                <Title apartment={data} />
                 <Details />
             </div>
             
