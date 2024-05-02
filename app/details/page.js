2f98f4611002c7e00d5ec8c70ef75abacdd3272e
Juan Miguel <jmcastrom@eafit.edu.co>
2024-05-02 17:33:05 -0500

    Resolving api conflicts

--- app/details/page.js
+++ app/details/page.js
@@ -2,8 +2,6 @@ import Details from "./Details";
 import Title from "./Title";
 
 export default async function Page() {
-    const res = await fetch('http://localhost:3000/api/search');
-    const data = await res.json();
 
     return (
         <div className="flex flex-col min-h-screen">
